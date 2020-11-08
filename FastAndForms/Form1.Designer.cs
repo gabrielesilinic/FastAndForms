7ed049fdbfd822913ca497ca0e411987988904b2
gabrielesilinic <gabriesilinic@gmail.com>
2020-11-08 22:40:28 +0100

    Aggiungere i file di progetto.

---
+++ FastAndForms/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FastAndForms
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctRoad0 = new System.Windows.Forms.PictureBox();
+            this.ptcRoad1 = new System.Windows.Forms.PictureBox();
+            this.ptcRoad2 = new System.Windows.Forms.PictureBox();
+            this.ptcRoad3 = new System.Windows.Forms.PictureBox();
+            this.pctCar1 = new System.Windows.Forms.PictureBox();
+            this.pctCar2 = new System.Windows.Forms.PictureBox();
+            this.lblWhoHasWon = new System.Windows.Forms.Label();
+            this.btnStartRace = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRoad0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRoad1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRoad2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRoad3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctRoad0
+            // 
+            this.pctRoad0.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pctRoad0.Image = global::FastAndForms.Properties.Resources.road_trip_background_10864211;
+            this.pctRoad0.InitialImage = null;
+            this.pctRoad0.Location = new System.Drawing.Point(0, 140);
+            this.pctRoad0.Name = "pctRoad0";
+            this.pctRoad0.Size = new System.Drawing.Size(247, 160);
+            this.pctRoad0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctRoad0.TabIndex = 0;
+            this.pctRoad0.TabStop = false;
+            // 
+            // ptcRoad1
+            // 
+            this.ptcRoad1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ptcRoad1.Image = global::FastAndForms.Properties.Resources.road_trip_background_10864211;
+            this.ptcRoad1.InitialImage = null;
+            this.ptcRoad1.Location = new System.Drawing.Point(244, 140);
+            this.ptcRoad1.Name = "ptcRoad1";
+            this.ptcRoad1.Size = new System.Drawing.Size(247, 160);
+            this.ptcRoad1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptcRoad1.TabIndex = 1;
+            this.ptcRoad1.TabStop = false;
+            // 
+            // ptcRoad2
+            // 
+            this.ptcRoad2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ptcRoad2.Image = global::FastAndForms.Properties.Resources.road_trip_background_10864211;
+            this.ptcRoad2.InitialImage = null;
+            this.ptcRoad2.Location = new System.Drawing.Point(486, 140);
+            this.ptcRoad2.Name = "ptcRoad2";
+            this.ptcRoad2.Size = new System.Drawing.Size(247, 160);
+            this.ptcRoad2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptcRoad2.TabIndex = 2;
+            this.ptcRoad2.TabStop = false;
+            // 
+            // ptcRoad3
+            // 
+            this.ptcRoad3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ptcRoad3.Image = global::FastAndForms.Properties.Resources.road_trip_background_10864211;
+            this.ptcRoad3.InitialImage = null;
+            this.ptcRoad3.Location = new System.Drawing.Point(730, 140);
+            this.ptcRoad3.Name = "ptcRoad3";
+            this.ptcRoad3.Size = new System.Drawing.Size(247, 160);
+            this.ptcRoad3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptcRoad3.TabIndex = 3;
+            this.ptcRoad3.TabStop = false;
+            // 
+            // pctCar1
+            // 
+            this.pctCar1.Image = global::FastAndForms.Properties.Resources.macchinabianca;
+            this.pctCar1.Location = new System.Drawing.Point(12, 156);
+            this.pctCar1.Name = "pctCar1";
+            this.pctCar1.Size = new System.Drawing.Size(100, 50);
+            this.pctCar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctCar1.TabIndex = 4;
+            this.pctCar1.TabStop = false;
+            this.pctCar1.Tag = "Macchina Nera";
+            // 
+            // pctCar2
+            // 
+            this.pctCar2.AccessibleDescription = "";
+            this.pctCar2.Image = global::FastAndForms.Properties.Resources.macchinarossa;
+            this.pctCar2.Location = new System.Drawing.Point(12, 231);
+            this.pctCar2.Name = "pctCar2";
+            this.pctCar2.Size = new System.Drawing.Size(100, 50);
+            this.pctCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctCar2.TabIndex = 5;
+            this.pctCar2.TabStop = false;
+            this.pctCar2.Tag = "Macchina Rossa";
+            // 
+            // lblWhoHasWon
+            // 
+            this.lblWhoHasWon.AutoSize = true;
+            this.lblWhoHasWon.Location = new System.Drawing.Point(456, 327);
+            this.lblWhoHasWon.Name = "lblWhoHasWon";
+            this.lblWhoHasWon.Size = new System.Drawing.Size(0, 13);
+            this.lblWhoHasWon.TabIndex = 6;
+            // 
+            // btnStartRace
+            // 
+            this.btnStartRace.Location = new System.Drawing.Point(428, 388);
+            this.btnStartRace.Name = "btnStartRace";
+            this.btnStartRace.Size = new System.Drawing.Size(111, 34);
+            this.btnStartRace.TabIndex = 7;
+            this.btnStartRace.Text = "Inizia";
+            this.btnStartRace.UseVisualStyleBackColor = true;
+            this.btnStartRace.Click += new System.EventHandler(this.btnStartRace_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 447);
+            this.Controls.Add(this.btnStartRace);
+            this.Controls.Add(this.lblWhoHasWon);
+            this.Controls.Add(this.pctCar2);
+            this.Controls.Add(this.pctCar1);
+            this.Controls.Add(this.ptcRoad3);
+            this.Controls.Add(this.ptcRoad2);
+            this.Controls.Add(this.ptcRoad1);
+            this.Controls.Add(this.pctRoad0);
+            this.Name = "frmMain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctRoad0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRoad1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRoad2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRoad3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctRoad0;
+        private System.Windows.Forms.PictureBox ptcRoad1;
+        private System.Windows.Forms.PictureBox ptcRoad2;
+        private System.Windows.Forms.PictureBox ptcRoad3;
+        private System.Windows.Forms.PictureBox pctCar1;
+        private System.Windows.Forms.PictureBox pctCar2;
+        private System.Windows.Forms.Label lblWhoHasWon;
+        private System.Windows.Forms.Button btnStartRace;
+    }
+}
+
---
+++ FastAndForms/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastAndForms
+{
+    public partial class frmMain : Form
+    {
+        enum Cars
+        {
+            car1,
+            car2
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        BackgroundWorker bgwcar1 = new BackgroundWorker();
+        BackgroundWorker bgwcar2 = new BackgroundWorker();
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.MaximumSize = this.Size;
+            this.MinimumSize += this.Size;
+            
+        }
+        void racestart()
+        {
+            if (bgwcar1.IsBusy||bgwcar2.IsBusy)
+            {
+                MessageBox.Show("Gara in corso");
+                return;
+            }
+            bgwcar1.WorkerReportsProgress = true;
+            bgwcar2.WorkerReportsProgress = true;
+            bgwcar1.DoWork += RndRace;
+            bgwcar2.DoWork += RndRace;
+            bgwcar1.ProgressChanged += RaceView;
+            bgwcar2.ProgressChanged += RaceView;
+            bgwcar1.RunWorkerCompleted += RaceEnd;
+            bgwcar2.RunWorkerCompleted += RaceEnd;
+            bgwcar1.WorkerSupportsCancellation = true;
+            bgwcar2.WorkerSupportsCancellation = true;
+            bgwcar1.RunWorkerAsync(pctCar1);
+            bgwcar2.RunWorkerAsync(pctCar2);
+        }
+        void RndRace(object sender, DoWorkEventArgs e)
+        {
+            var rnd = new Random();
+            
+            var currbgw = sender as BackgroundWorker;
+            var currpct = e.Argument as PictureBox;
+            int i;
+            for (i = 0; i < this.Size.Width-currpct.Width/2; i++)
+            {
+                if (currbgw.CancellationPending)
+                {
+                    e.Result = (false, i,"");
+                    return;
+                }
+                i += rnd.Next(1, 20);
+                currbgw.ReportProgress(i, e.Argument);
+                Thread.Sleep(100);
+            }
+            e.Result = (true, i,currpct.Tag as string);
+        }
+        void RaceView(object sender, ProgressChangedEventArgs e)
+        {
+            var imgbx = e.UserState as PictureBox;
+            imgbx.Location = new Point(e.ProgressPercentage, imgbx.Location.Y);
+        }
+        void RaceEnd(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var (completed, res,carname) = (ValueTuple<bool, int, string>)e.Result;
+            if (!completed)
+            {
+                Console.WriteLine(carname + " cancellato");
+                return;
+            }
+            lblWhoHasWon.Text = carname + " ha vinto";
+            Console.WriteLine(carname + " completato");
+            if (bgwcar1.IsBusy)
+            { bgwcar1.CancelAsync(); }
+            if (bgwcar2.IsBusy)
+            { bgwcar2.CancelAsync(); }
+        }
+
+        private void btnStartRace_Click(object sender, EventArgs e)
+        {
+            racestart();
+        }
+    }
+}
